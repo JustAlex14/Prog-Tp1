6f3c4112d60253c32c684de3fb4a1f6b7954d91f
Alex <98466389+JustAlex14@users.noreply.github.com>
2023-09-20 11:43:06 +0200

    Mise en forme (question 2.1)

--- tp1/src/app/page.tsx
+++ tp1/src/app/page.tsx
@@ -1,8 +1,37 @@
 import Image from 'next/image'
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import {ProductCardLayout, ProductGridLayout} from "tp-kit/components/products";
+import {BreadCrumbs, Button, SectionContainer} from "tp-kit/components";
+const categories = PRODUCTS_CATEGORY_DATA;
 
 export default function Home() {
   return (
     <main>
+      <BreadCrumbs
+          className=""
+          items={[
+            {
+              label: 'Accueil',
+              url: '#'
+            }
+          ]}
+      />
+        {categories.map(category =>
+            <SectionContainer>
+              <h1>{category.name} ({category.products.length})</h1>
+              <ProductGridLayout
+                  products={category.products}
+              >
+                  {product =>
+                      <ProductCardLayout
+                          button={<Button variant="ghost" fullWidth="true">Ajouter au panier</Button>}
+                          product={product}
+                      />
+                  }
+
+              </ProductGridLayout>
+            </SectionContainer>
+        )}
     </main>
   )
 }
