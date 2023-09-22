2109fb38412e5f483bca4cac28c1a64b2a795eef
Alex <98466389+JustAlex14@users.noreply.github.com>
2023-09-22 15:42:57 +0200

    fin séance 2

---
+++ tp1/src/app/[categorySlug]/[productSlug]/page.tsx
@@ -0,0 +1,68 @@
+
+import ProductFilters from "@/components/product-filters";
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import { BreadCrumbs } from "../../../../tp-kit/components/breadcrumbs";
+import { Button } from "../../../../tp-kit/components/button";
+import { ProductCardLayout } from "../../../../tp-kit/components/products/product-card-layout";
+import { ProductGridLayout } from "../../../../tp-kit/components/products/product-grid-layout";
+import { SectionContainer } from "../../../../tp-kit/components/section-container";
+
+const categories = PRODUCTS_CATEGORY_DATA;
+
+type NextPageProps<T = Record<string, string>> = {
+  
+  params: T,
+  
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+// Ce que j'attends comme paramètres
+type Props = {
+	categorySlug: string,
+	productSlug: string,
+}
+
+// app/blog/[category]/[post]/page.tsx
+export default function BlogPostPage({params} : NextPageProps<Props>) {
+	const categ = categories.filter(category => category.slug.toUpperCase() == params.categorySlug.toUpperCase())[0]
+	const productList = categ.products.filter(products => products.slug.toUpperCase() != params.productSlug.toUpperCase)
+	const product = categ.products.filter(products => products.slug == params.productSlug)[0]
+	//const blogPost = await getPostWithinCategory(params.category, params.post);
+
+	// Retourne une 404 si non trouvé
+
+	return <>
+		<BreadCrumbs
+            className=""
+            items={[
+              {
+                label: 'Accueil',
+                url: '../../'
+              },
+			  {
+				label: categ.name,
+				url: '../'
+			  },
+			  {
+				label: product.name,
+				url: '#'
+			  }
+            ]}
+        />
+		<SectionContainer>
+			<h1 className="mb-3 text-xl"><b>{categ.name} ({categ.products.length})</b></h1>
+			{console.log(product)}
+			<ProductGridLayout
+				products={productList}
+				>
+				{product =>
+					<ProductCardLayout
+						button={<Button variant="ghost" fullWidth="true">Ajouter au panier</Button>}
+						product={product}
+					/>
+				}
+
+			</ProductGridLayout>
+		</SectionContainer>
+	</>;
+}
